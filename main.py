c6560a063613cee1321a4f1f0b8224186962e461
Bhushan <bhushan1810patil@gmail.com>
2021-06-30 16:08:05 +0530

    mid codes, added font

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 from settings import *
+from colors import *
 
 class Grid:
     
@@ -15,10 +16,11 @@ class Grid:
         self.cols = COLS
         self.fps = FPS
         self.clock = None
-        self.font = None
+        self.titleFont = None
         self.grid = None
         self.win = None
         self.gameWin = None
+        self.gameWinRect = None
 
         
     def grid_init(self):
@@ -28,11 +30,20 @@ class Grid:
         self.win = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Game of Life")
         
-        self.gameWin = self.win.subsurface((self.xoff, self.yoff, self.gameWinWidth, self.gameWinHeight))
+        self.gameWinRect = pygame.Rect(self.xoff, self.yoff, self.gameWinWidth, self.gameWinHeight)
+        self.gameWin = self.win.subsurface(self.gameWinRect)
         
-        self.win.fill((30, 20, 50))
-        self.gameWin.fill((40, 102, 139))
+        self.win.fill(MID_BLACK)
+        self.gameWin.fill(STEEL_BLUE)
         
+        self.titleFont = pygame.font.SysFont(TITLE_FONT, FONT_SIZE)
+        title = self.titleFont.render("Game of Life", 1, GOLD)
+        w, h = title.get_size()
+        blitX = (self.width - w) // 2
+        blitY = (self.yoff - h) // 2
+        self.win.blit(title, (blitX, blitY))
+                
+        self.clock = pygame.time.Clock()
         pygame.display.update()
     
     def close(self):
@@ -48,9 +59,8 @@ class Grid:
             
         run = True
         while run:
-            
+            self.clock.tick(self.fps)
             for event in pygame.event.get():
-                
                 if event.type == pygame.QUIT:
                     run = False
                     
