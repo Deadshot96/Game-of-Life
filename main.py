0ba1a531d7a74148ee23731eb8b24f66918f4a09
Bhushan <bhushan1810patil@gmail.com>
2021-06-30 14:57:11 +0530

    created main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+import random
+
+class Grid:
+    
+    def __init__(self):
+        pass
+    
+    def run(self):
+        pass
+    
+    
+if __name__ == "__main__":
+    print("Hello, World!")
